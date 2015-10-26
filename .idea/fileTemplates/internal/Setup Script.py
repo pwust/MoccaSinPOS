751e4095322b88da8d92dd2cad34b2305006b91b
pwust <patric.wust@gmx.de>
2015-10-26 23:40:07 +0100

    new initial commit

---
+++ .idea/fileTemplates/internal/Setup Script.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='$Package_name',
+    version='$Version',
+    packages=$PackageList,$PackageDirs
+    url='$URL',
+    license='$License',
+    author='$Author',
+    author_email='$Author_Email',
+    description='$Description'
+)
