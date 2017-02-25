e652b9102587aca55ce1777c0cb02e31b10ace20
pwust <patric.wust@gmx.de>
2017-02-25 16:00:49 +0100

    before changing labelframes to frames

--- src/place-ansatz.py
+++ src/place-ansatz.py
@@ -24,135 +24,7 @@ logger = logging.getLogger('button-grid')
 GLOBAL_BG = '#0B660B'
 
 
-class CashPointTester(tk.Tk):
-
-    logger = logging.getLogger('button-app')
-
-    def __init__(self, parent=None, rows=12, cols=10):
-        tk.Tk.__init__(self, parent)
-        self.rows = rows
-        self.cols = cols
-        self.init_app()
-
-    def recalc_sizes(self):
-        '''
-        Recalculate all relevant sizes for the GUI
-        :return:
-        '''
-        pass
-
-    def init_app(self):
-        for x in range(self.cols):
-            for y in range(self.rows):
-                cmd = lambda x=x, y=y: self.button_callback(x, y)
-                b = tk.Button(self, text='{}/{}'.format(x, y), command=cmd)
-                b.grid(row=y, column=x)
-                if (x == (self.cols - 1)) and (y == (self.rows - 1)):
-                    b.configure(text='Exit')
-
-    def button_callback(self, x, y):
-        logger.debug('button {:2}/{:2} pressed.'.format(x, y))
-
-
-class CashPointGridTest1(tk.Tk):
-
-    def __init__(self, parent=None, rows=12, cols=10, numerator=3, denominator=4, center=True):
-        tk.Tk.__init__(self, parent)
-        self.rows = rows
-        self.cols = cols
-        self.numerator = numerator
-        self.denominator = denominator
-        self.center = center
-        self.init_app()
-
-    def init_app(self):
-        # determine screen size
-        screen_width = self.winfo_screenwidth()
-        screen_height = self.winfo_screenheight()
-        logger.debug('screen size = %sx%s' % (screen_width, screen_height))
-
-        logger.debug('factor for window size used: {}/{}, centering = {}'
-                     .format(self.numerator, self.denominator, self.center))
-
-        window_width = self.numerator * screen_width // self.denominator  # 3/4 of the screen size used
-        window_height = self.numerator * screen_height // self.denominator  # 3/4 of the screen size used
-        logger.debug('window size = %sx%s' % (window_width, window_height))
-
-        if self.center:
-            offset_x = (screen_width - window_width) // 2  # centered
-            offset_y = (screen_height - window_height) // 2  # centered
-        else:
-            offset_x = offset_y = 0
-        logger.debug('offset      = %sx%s' % (offset_x, offset_y))
-
-        col_width = window_width // self.cols
-        row_height = window_height // self.rows
-        logger.debug('button size = %sx%s' % (col_width, row_height))
-
-        # self.geometry(newGeometry='{}x{}+{}+{}'.format(window_width, window_height, offest_x, offset_y))
-
-        # move window to center of available screen
-        self.geometry(newGeometry='+{}+{}'.format(offset_x, offset_y))
-
-        # self.minsize(width=window_width//2, height=window_height//2)
-        # self.maxsize(width=screen_width, height=screen_height)
-
-        # set window to a fixed size:
-        self.minsize(width=window_width, height=window_height)
-        self.maxsize(width=window_width, height=window_height)
-        self.resizable(False, False)
-
-        # make window borderless:
-        # self.overrideredirect(1)
-
-        # set background to dark green:
-        self.tk_setPalette('#0B660B')
-
-        tk.Grid.rowconfigure(self, 0, weight=1)
-        tk.Grid.columnconfigure(self, 0, weight=1)
-
-        # Create & Configure frame
-        frame = tk.Frame(self)
-        frame.grid(row=0, column=0, sticky=tk.N + tk.S + tk.E + tk.W)
-
-        # Create a 12x10 (default rows x columns) grid of buttons inside the frame
-        for col_index in range(self.cols):
-            tk.Grid.columnconfigure(frame, col_index, weight=1)
-            for row_index in range(self.rows):
-                tk.Grid.rowconfigure(frame, row_index, weight=1)
-                # Exceptions where not to put a Button:
-                if ((row_index != 3) and (row_index != 2)) \
-                        or (((row_index == 3) or (row_index == 2))
-                            and ((col_index == 0) or (col_index == self.cols - 1))):
-                    cmd = lambda x=col_index, y=row_index: self.button_callback(x, y)
-                    btn = tk.Button(frame, )
-                    btn.configure(text='{}/{}'.format(col_index, row_index))
-                    btn.configure(command=cmd)
-                    btn.configure(background='#333')
-                    btn.configure(width=col_width, height=row_height)
-                    # btn.configure(padx=20, pady=4)
-                    btn.grid(row=row_index, column=col_index, sticky=tk.N + tk.S + tk.E + tk.W)
-                    if (row_index == (self.rows - 1)) and (col_index == (self.cols - 1)):
-                        btn.config(text='EXIT')
-                lbl = tk.Label(frame, text='I am a label text, and I wonder how long I can get...')
-                # lbl = tk.Message(frame, text='I am a label text, and I wonder how long I can get...')
-                lbl.configure(anchor=tk.W)
-                lbl.configure(background='white')
-                lbl.configure(font=('Helvetica', 20, 'bold'))
-                lbl.grid(row=2, column=1, columnspan=8, rowspan=2, sticky=tk.N + tk.S + tk.E + tk.W)
-
-    def button_callback(self, x, y):
-        logger.debug('button {:2}/{:2} pressed.'.format(x, y))
-        if (x == (self.cols - 1)) and (y == (self.rows - 1)):
-            logger.info('Pressed "EXIT" -> exiting.')
-            self.clicked_exit()
-
-    def clicked_exit(self):
-        logger.debug('clicked_exit()')
-        self.destroy()
-
-
-class CashPointGridTest2(tk.Tk):
+class CashPointTest(tk.Tk):
 
     def __init__(self, parent=None, rows=12, cols=10, numerator=3, denominator=4, center=True):
         tk.Tk.__init__(self, parent)
@@ -191,17 +63,16 @@ class CashPointGridTest2(tk.Tk):
         self.geometry(newGeometry='{}x{}+{}+{}'.format(window_width, window_height, offset_x, offset_y))
 
         # move window to center of available screen
-#        self.geometry(newGeometry='{}x{}+{}+{}'.format(window_width, window_height, offset_x, offset_y))
+        #        self.geometry(newGeometry='{}x{}+{}+{}'.format(window_width, window_height, offset_x, offset_y))
 
-        self.minsize(width=window_width, height=window_height)
+        self.minsize(width=window_width // 2, height=window_height // 2)
         self.maxsize(width=screen_width, height=screen_height)
 
         # set window to a fixed size:
-#        self.minsize(width=window_width, height=window_height)
-#        self.maxsize(width=screen_width, height=screen_height)
+        #        self.minsize(width=window_width, height=window_height)
+        #        self.maxsize(width=screen_width, height=screen_height)
         # self.maxsize(width=window_width, height=window_height)
         # self.resizable(False, False)
-
 
         # make window borderless:
         # self.overrideredirect(1)
@@ -217,12 +88,11 @@ class CashPointGridTest2(tk.Tk):
         # gui_style.configure()
         # gui_style.configure('TButton', font='Courier New')
         gui_style.configure('TLabelframe', background=GLOBAL_BG)
-        logger.debug('Style for TLabelframe -> font: {}'.format(ttk.Style().lookup("TLabelframe", "font")))
-        logger.debug('Style for TButton -> width: {}'.format(ttk.Style().lookup("TButton", "width")))
+        # logger.debug('Style for TLabelframe -> font: {}'.format(ttk.Style().lookup("TLabelframe", "font")))
+        # logger.debug('Style for TButton -> width: {}'.format(ttk.Style().lookup("TButton", "width")))
 
         # logger.debug('Themes available: {}'.format(gui_style.theme_names()))
         # logger.debug('Elements available: {}'.format(gui_style.element_names()))
-
 
         # set background to dark green:
         self.tk_setPalette(GLOBAL_BG)
@@ -232,45 +102,49 @@ class CashPointGridTest2(tk.Tk):
 
         # Create & Configure frame
         frm_app = ttk.Frame(self, style='My.TFrame')
-        # frmApp.grid(row=0, column=0, sticky=tk.N + tk.S + tk.E + tk.W)
-
 
         # putting lableframes for POS GUI:
-        frm_app.grid(row=0, column=0)
+        frm_app.grid(row=0, column=0, sticky='nesw')
+        # frm_app.grid_propagate(0)
+        logger.debug('frm_app: {}'.format(frm_app.grid_info()))
 
         # TOP AREA: Logo, Menu Button, Receipt Display,
 
         frm_display = ttk.LabelFrame(frm_app, text=' Top Area ')
-        frm_display.grid(column=0, row=0, columnspan=3, sticky=tk.E + tk.W)
+        frm_display.grid(column=0, row=0, columnspan=3, rowspan=1, sticky='ew')
+        # frm_display.grid_propagate(0)
+        logger.debug('frm_display: {}'.format(frm_display.grid_info()))
+
         logger.debug('frm_display is of class {}'.format(frm_display.winfo_class()))
         ttk.Button(frm_display, text='EXIT', command=self.clicked_exit).grid(column=0, row=0)
 
         frm_1st_buttons = ttk.LabelFrame(frm_app, text=' Level 1 ')
-        frm_1st_buttons.grid(column=0, row=1, rowspan=2, sticky=tk.N + tk.S)
+        frm_1st_buttons.grid(column=0, row=1, rowspan=2, sticky='ns')
+        logger.debug('frm_1st_buttons: {}'.format(frm_1st_buttons.grid_info()))
         for x in range(6):
             for y in range(8):
                 ttk.Button(frm_1st_buttons, text="{}/{}".format(x, y)).grid(column=x, row=y)
 
         frm_2nd_buttons = ttk.LabelFrame(frm_app, text=' Level 2 ')
         frm_2nd_buttons.grid(column=1, row=1)
+        logger.debug('frm_2nd_buttons: {}'.format(frm_2nd_buttons.grid_info()))
         for x in range(3):
             for y in range(7):
                 ttk.Button(frm_2nd_buttons, text="{}/{}".format(x, y)).grid(column=x, row=y)
 
         frm_user_display = ttk.LabelFrame(frm_app, text=' User Info ')
-        frm_user_display.grid(column=1, row=2, sticky=tk.W + tk.E)
+        frm_user_display.grid(column=1, row=2, sticky='ew')
+        # frm_user_display.grid_propagate(0)
+        logger.debug('frm_user_display: {}'.format(frm_user_display.grid_info()))
         # ttk.Button(frm_user_display, text='tmpBtn').grid(column=0, row=0)
-        ttk.Label(frm_user_display, text='Angemeldeter Bediener:').grid(column=0, row=0, sticky=tk.W+tk.E)
-        lbl_active_user = ttk.Label(frm_user_display, text='BEDIENER ???')
-        lbl_active_user.grid(column=0, row=1, sticky=tk.W+tk.E)
+        ttk.Label(frm_user_display, text='Angemeldeter Bediener:').grid(column=0, row=0, sticky='ew')
+        lbl_active_user = ttk.Label(frm_user_display, text='BEDIENER - ??')
+        lbl_active_user.grid(column=0, row=1, sticky='ew')
         lbl_active_user.configure(style='Userinfo.TLabel')
 
-
-
-
-
         frm_3rd_buttons = ttk.LabelFrame(frm_app, text=' Level 3 ')
-        frm_3rd_buttons.grid(column=2, row=1, rowspan=2, sticky=tk.N + tk.S)
+        frm_3rd_buttons.grid(column=2, row=1, rowspan=2, sticky='ns')
+        logger.debug('frm_3rd_buttons: {}'.format(frm_3rd_buttons.grid_info()))
         ttk.Button(frm_3rd_buttons, text='tmpBtn').grid(column=0, row=0, columnspan=2)
 
 
@@ -323,7 +197,7 @@ class CashPointGridTest2(tk.Tk):
 def main():
     # CashPointTester().mainloop()
     # CashPointGridTest1(numerator=1, denominator=2, center=False).mainloop()
-    CashPointGridTest2(numerator=2, denominator=3, center=True, rows=5, cols=5).mainloop()
+    CashPointTest(numerator=2, denominator=3, center=True, rows=5, cols=5).mainloop()
 
 if __name__ == '__main__':
     main()
