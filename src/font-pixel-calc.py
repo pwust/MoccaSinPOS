59315bd9e03b610038b8f0edd8748c353c83de8b
pwust <patric.wust@gmx.de>
2017-03-13 00:31:03 +0100

    font metrics tests continued

--- src/font-pixel-calc.py
+++ src/font-pixel-calc.py
@@ -16,7 +16,9 @@ logger = logging.getLogger('font-pixel-calc')
 
 def main():
     root = tk.Tk()
-    canvas = tk.Canvas(root, width=728, height=600)  # pixels?
+    my_window_width = 728
+    my_window_height = 600
+    canvas = tk.Canvas(root, width=my_window_width, height=my_window_height)  # pixels?
     canvas.pack()
     (x, y) = (5, 5)
     text = 'yellow world'
@@ -30,18 +32,23 @@ def main():
     #     fonts.append(font)  # save font values from garbage collection
     #     y += h + 5
 
-    my_text = 'n' * 10
-    my_other_text = 'm' * 10
+    my_texts = []
+    my_texts.append('n' * 10)
+    # my_texts.append('m' * 10)
+
     for my_size in range(10, 40, 2):
         my_font = tkfont.Font(family='Arial Narrow', size=my_size)
-        for my_actual_text in (my_text, my_other_text):
-            # my_font = tkfont.Font(family='Lucida Console', size=my_size)
-            my_width = my_font.measure(my_actual_text)
-            my_height = my_font.metrics('linespace')
-            canvas.create_text(x, y, text='{} ({}pt)'.format(my_actual_text, my_size), font=my_font, anchor='nw')
-            logger.debug('size = {}pt, height = {}px, width of "{}" = {}px'.format(my_size, my_height, my_actual_text, my_width))
-            y += my_height + 5
-            logger.debug('y = {}, window height = {}'.format(y, canvas.winfo_height()))
+        for my_actual_text in my_texts:
+            if y <= my_window_height:
+                # my_font = tkfont.Font(family='Lucida Console', size=my_size)
+                my_width = my_font.measure(my_actual_text)
+                my_height = my_font.metrics('linespace')
+                if y + my_height <= my_window_height:
+                    canvas.create_text(x, y, text='{} ({}pt)'.format(my_actual_text, my_size), font=my_font, anchor='nw')
+                    logger.debug('size = {}pt, height = {}px, width of "{}" = {}px'.format(my_size, my_height, my_actual_text, my_width))
+                    y += my_height + 5
+                    logger.debug('y = {}, window height = {}'.format(y, my_window_height))
+
 
     tk.mainloop()
 
